38f9116599206760c63fa706fe7a2f35a24ed49f
hanyonghee <hanyonghee9264@gmail.com>
2018-10-01 14:47:00 +0900

    post_create에서 POST요청을 처리한 후 /blog-posts/로 redirect

--- app/blog/views.py
+++ app/blog/views.py
@@ -2,7 +2,7 @@ import os
 import random
 import re
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
 from .models import Post
@@ -44,7 +44,27 @@ def post_create(request):
     """
     if request.method == 'POST':
         # POST요청이 왔을 경우
-        # 새 글을 작성하고 원하는 페이지로 돌아가도록 함
-        pass
+        # 새 글을 작성하고 원하는 페이지로 돌아가도록
+
+        # 데이터는 request.POST안에 있음
+        # HttpResponse를 돌려줌
+        # 제목: <제목데이터><br>내용: <내용데이터>
+        # 위 문자열을 가지고 response돌려주기
+        title = request.POST['title']
+        text = request.POST['text']
+
+        # objects.create() 메서드를 사용해서
+        # 새 Post객체를 생성하며 DB에 저장 (create() 실행의 반환값은 'post'변수에 할당)
+        #  title, text는 request.POST에서 가져온 내용
+        #  author는 request.user
+        # 리턴하는 결과는 같은 문자열이지만,
+        #  문자열을 생성할 때 만들어진 Post객체('post'변수)의 title속성, text속성을 사용
+        post = Post.objects.create(
+            author=request.user,
+            title=title,
+            text=text,
+        )
+        next_path = '/blog-posts/'
+        return HttpResponseRedirect(next_path)
     else:
         return render(request, 'blog/post_create.html')
